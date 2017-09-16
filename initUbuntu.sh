0ef5c890994409627d8b3e0740cc9aaa2672ddbe
chendan <chendan316@gmail.com>
2017-09-16 11:40:45 +0800

    init

---
+++ initUbuntu.sh
@@ -0,0 +1,12 @@
+apt-get update
+apt-get install vim
+apt-get install git
+apt-get install zsh
+chsh -s /bin/zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# 拷贝zsh配置
+mv .zshrc.conf .zshrc
+
+
+
+
