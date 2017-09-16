990c766a6bbde743c50a9500931a89919c017a45
chendan <chendan316@gmail.com>
2017-09-16 17:24:17 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # autoWork
 自动化工作脚本
+
+### 常用jetbrains插件
+- igonre
+- BashSupport
+- IdeaVim
+
--- initUbuntu.sh
+++ initUbuntu.sh
@@ -5,8 +5,11 @@ apt-get install zsh
 chsh -s /bin/zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 # 拷贝zsh配置
-mv .zshrc.conf .zshrc
+cp -f .zshrc.conf .zshrc
 
-
-
-
+# 安装php依赖
+apt-get -y autoconf bison re2c
+wget https://github.com/php/php-src/archive/php-7.1.9.tar.gz
+tar -zxvf php-7.1.9.tar.gz
+cd php-7.1.9
+./buildconf --force
