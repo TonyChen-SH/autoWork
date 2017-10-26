e9a76aa172d8e2ab926126ccbab9f571f14ba9b2
TonyChen-SH <chendan316@gmail.com>
2017-10-26 18:18:56 +0800

    Update backMysql.sh

--- backMysql.sh
+++ backMysql.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # declare an array called array and define 3 vales
-dbNames=( rog_log_s216 )
+dbNames=( tos_log_iqiyi_s3 tos_log_iqiyi_s4 )
 for dbName in "${dbNames[@]}"
 do
-     mysqldump -h127.0.0.1 -uroot -p123456 ${dbName}>${dbName}.sql
-     tar -zcvf ${dbName}.tar.gz ${dbName}.sql
-     rm -rf ${dbName}.sql
+     mysqldump -h127.0.0.1 -uroot -p123456 ${dbName}>${dbName}.sql && tar -zcvf ${dbName}.tar.gz ${dbName}.sql
+     rm -rf ${dbName}.sqlls
 done
